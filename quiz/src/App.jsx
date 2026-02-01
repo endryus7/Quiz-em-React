44ab49a5e2cba2076469750b82dd01a191fcc666
Endryus-Montezano <endryusmontezano@live.com>
2026-02-01 19:11:45 -0300

    Configuração inicial do projeto de quiz em React

---
+++ quiz/src/App.jsx
@@ -0,0 +1,18 @@
+// welcome
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <h1>Quiz App</h1>
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
