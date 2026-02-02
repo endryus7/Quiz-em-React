3c934a34c787d9e5691488adc97036ccbe013cee
Endryus-Montezano <endryusmontezano@live.com>
2026-02-02 19:36:34 -0300

    Adiciona tela de fim de jogo e css

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -3,9 +3,9 @@ import { QuizContext } from './context/quiz'
 
 import Welcome from './components/Welcome'
 import Question from './components/Question'
+import GameOver from './components/GameOver'
 
 import './App.css'
-
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext)
@@ -20,6 +20,7 @@ function App() {
       <h1>Quiz App</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   )
 }
