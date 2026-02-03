09f2ea4744852fcb1aeeb3f84bebcddb343518a7
Endryus-Montezano <endryusmontezano@live.com>
2026-02-03 02:36:05 -0300

    Adiciona funcionalidade de eliminar opção

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -28,12 +28,14 @@ const Question = () => {
               key={option} 
               answer={currentQuestion.answer}
               selectOption={() => onSelectOption(option)}
+              hide={quizState.optionToHide === option ? "hide" : null}
               />
             ))}
         </div>
         {!quizState.answerSelected && !quizState.help && (
           <>
             {currentQuestion.tip && <button onClick={() => dispatch({ type: "SHOW_TIP"})}>Dica</button>}
+            <button onClick={() => dispatch({ type: "REMOVE_OPTION"})}>Excluir uma</button>
           </>
         )}
         {!quizState.answerSelected && quizState.help === 'tip' && (
